4bb9ece4019802d45f8fbcec867da1a5752375dd
Chloe Gao <cgao24@uw.edu>
2025-07-29 16:40:51 -0700

    Initial React app setup with Create React App

---
+++ js-basics/todo-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My To-Do List</h1>
+        <p>Ready to build something awesome!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
